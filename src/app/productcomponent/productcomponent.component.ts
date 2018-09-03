ec1f30208d69d618cb112a71ab93c840434f7bae
benjith <mpbenjith@gmail.com>
2018-09-03 16:01:26 +0530

    first commit

---
+++ src/app/productcomponent/productcomponent.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-productcomponent',
+  templateUrl: './productcomponent.component.html',
+  styleUrls: ['./productcomponent.component.css']
+})
+export class ProductcomponentComponent implements OnInit {
+  productNames =['Men','Women','Kids'];
+  hiddenID=[{value:-1,name:''}];
+  favValue:number=0;
+  cartValue=0;
+  btnStyle:string;
+  addToCartItem:string='';
+  
+ allImgaesurls=[
+                 {id:'1',Name:'Royal Enfeild',Category:'imgMen',url:'../../assets/images/t (6).jpg'},
+                 {id:'2',Name:'T Shirt',Category:'imgMen',url:'../../assets/images/t (5).jpg'},
+                 {id:'3',Name:'Royal Enfeild',Category:'imgMen',url:'../../assets/images/t (2).jpg'},
+                 {id:'4',Name:'Royal Enfeild',Category:'imgMen',url:'../../assets/images/t (2).jpg'},
+                 {id:'5',Name:'Royal Enfeild',Category:'imgMen',url:'../../assets/images/t (2).jpg'},
+                 {id:'6',Name:'Royal Enfeild',Category:'imgMen',url:'../../assets/images/t (2).jpg'},
+             
+  ]
+  
+  arrAddtoFav=[];
+  arrAddtoCart=[];
+  constructor() {this.btnStyle="glyphicon glyphicon-heart-empty"; }
+ 
+
+  ngOnInit() {
+  }
+
+  showItem(itemId:number,name:string) {
+    this.hiddenID=[{value:-1,name:''}];
+    this.hiddenID['value']=itemId;
+    this.hiddenID['name']=name;
+    alert(this.hiddenID['name']);
+  }
+  
+ favCount(){
+  return this.favValue=this.favValue+1;
+ }
+ addToCart(e){
+   this.arrAddtoCart.push(e);
+   return this.cartValue=this.cartValue+1;
+ }
+ remocveCart(e){
+  this.arrAddtoCart.splice(this.arrAddtoCart.indexOf(e),1);
+  return this,this.cartValue=this.cartValue-1;
+ }
+ displayAllCartItem(){
+   alert(this.arrAddtoCart);
+ }
+
+
+ checkItemInFav(id)
+ {
+   if(this.arrAddtoFav.indexOf(id)==-1)
+   {
+    console.log(id);
+     return false;
+     
+   }
+   else
+   {
+    console.log(id);
+     return true;
+   }
+ }
+ addToFav(id)
+ {
+  this.favCount();
+  this.arrAddtoFav.push(id);
+
+ }
+ removeArrFav(id)
+ {
+  let i=this.arrAddtoFav.indexOf(id);
+  this.arrAddtoFav.splice(i,1);
+  this.favValue=this.favValue-1;
+ }
+ checkItemInCart(id)
+ {
+   if(this.arrAddtoCart.indexOf(id)==-1)
+   {
+    return false;
+   }
+   else{
+    return true;
+   }
+ }
+
+
+}
